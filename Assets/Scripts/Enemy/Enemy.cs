4e14b1d154ad4d86305677966fd8f27f797ccb0d
Matthew YON <matthew_yon@ens.univ-artois.fr>
2021-01-16 19:18:12 +0100

    Ajout de la barre de vie ennemies

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour
 {
     [SerializeField] int Health = 100;
     [SerializeField] GameObject item;
+    [SerializeField] Slider slider;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        slider = GetComponentInChildren<Slider>();
+        slider.maxValue = Health;
+        slider.value = Health;
     }
 
     // Update is called once per frame
@@ -26,13 +30,14 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(int damage)
     {
         Health -= damage;
+        slider.value = Health;
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag == "Player")
         {
-            TakeDamage(other.gameObject.GetComponent<PlayerStats>().getDamage());
+            TakeDamage(other.gameObject.GetComponentInParent<PlayerStats>().getDamage());
         }
     }
 }
--- Assets/Scripts/Player/AttackScript.cs
+++ Assets/Scripts/Player/AttackScript.cs
@@ -5,47 +5,29 @@ using UnityEngine;
 public class AttackScript : MonoBehaviour
 {
     private Animator anim;
-    [SerializeField] int hitRange = 1;
+    private MeshCollider mc;
 
     private void Awake()
     {
         anim = GetComponentInChildren<Animator>();
+        mc = GetComponentInChildren<MeshCollider>();
     }
-
-    private void FixedUpdate()
-    {
-        
-    }
-
 
     public void OnAttack()
     {
         anim.ResetTrigger("isWalking");
         anim.ResetTrigger("isIdling");
         anim.SetTrigger("isAttacking");
+    }
 
-        
-        int layerMask = 1 << 8;
-
-        layerMask = ~layerMask;
-
-        RaycastHit hit;
-        Vector3 origin = transform.position;
-        origin.y += 2;
-
-        if (Physics.Raycast(origin, transform.TransformDirection(Vector3.forward), out hit, hitRange, layerMask))
-        {
-            Debug.DrawLine(origin, hit.transform.position, Color.red);
-            if (hit.transform.gameObject.tag == "Enemy")
-            {
-                hit.transform.gameObject.SendMessage("TakeDamage", 30);
-            }
-        }
-        
+    public void Slash()
+    {
+        mc.enabled = true;
     }
 
     private void EndAttack()
     {
         anim.ResetTrigger("isAttacking");
+        mc.enabled = false;
     }
 }
